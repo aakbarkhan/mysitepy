8597c5d3903a5a35203680a38e40add0df30c9ab
aakbarkhan <aakbarkhan@gmail.com>
2022-02-18 16:25:15 +0530

    Add gitignore

--- newsite/settings.py
+++ newsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f_evw#0=x&vew@rb5#$$!z)69wcj=1e3g9n-n9$fw-8jtq6f%&'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+HTML_STR = '''
+<h1>Hello World</h1>
+'''
+
+def home_view(request):
+    return HttpResponse(HTML_STR)
